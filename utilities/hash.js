715053ddd2c81e0c6cb9b2f8f6168c047b89e979
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 00:23:36 +0100

    annotating hash.js

--- utilities/hash.js
+++ utilities/hash.js
@@ -1,11 +1,24 @@
 const bcrypt = require('bcrypt');
 
 // using bcrypt to hash
+
+/**
+ * @param {string} password Password
+ * @returns { Promise<pending> }
+ */
+
 const hashPassword = async (password) => {
     return await bcrypt.hash(password, 10);
 };
 
+
 // using bcrypt to compare
+/**
+ * @param {string} password Password
+ * @param {string} passwordhash Passwordhash
+ * @returns { Promise<pending> }
+ */
+
 const comparePassword = async (password, passwordhash) => {
     return await bcrypt.compare(password, passwordhash);
 };
