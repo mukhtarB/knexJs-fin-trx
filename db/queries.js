403202d30a9649a93a5671c7dfaec34f439f2850
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-13 18:41:59 +0100

    removed console log

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,6 @@
 const knex = require('./knex');
 
 const insert = (table, objValue) => {
-    console.log(objValue)
     if (Object.prototype.toString.call(objValue) !== '[object Object]') throw new Error ('value must be object');
     
     return knex(`${table}`).insert(objValue)
--- utilities/generateToken.js
+++ utilities/generateToken.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 const {MD5} = require('crypto-js');
 
 // generating faux token for the purposes of implementation
-const generateToken = (hash) => {
+const generateToken = (hashing) => {
     let secretKeySalt = MD5(process.env.secretKey)
-    let shuffleToken = hash+secretKeySalt;
+    let shuffleToken = hashing+secretKeySalt;
     return [...shuffleToken].sort(()=>Math.random()-.5).join('');
 };
 
-module.exports = generateToken;
+module.exports = {generateToken};
