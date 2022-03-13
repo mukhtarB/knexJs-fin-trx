7b722410ad5684cf039b75aa5ad24af41c75cb17
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-13 21:40:39 +0100

    annotating query.js

--- db/queries.js
+++ db/queries.js
@@ -1,17 +1,32 @@
 const knex = require('./knex');
 
-const insert = (table, objValue) => {
-    if (Object.prototype.toString.call(objValue) !== '[object Object]') throw new Error ('value must be object');
+/**
+ * @param {string} table The Table Name
+ * @param {object} data The table data (object)
+ * @returns { Promise<pending> } unresolved Promise
+ */
+
+const insert = (table, data) => {
+    if (Object.prototype.toString.call(data) !== '[object Object]') throw new Error ('value must be object');
     
-    return knex(`${table}`).insert(objValue)
+    return knex(`${table}`).insert(data)
     .then( () => knex(`${table}`) )
     .then(resultSet => resultSet.pop())
     .catch(err => err);
 };
 
+
+/**
+ * @param {string} table The Table Name
+ * @param {string} col The Column
+ * @param {string} val The stringValue
+ * @returns { Promise<pending> }
+ */
+
 const selectOne = (table, col, val) => {
     return knex(table).where(col, val).first();
 };
+
 
 module.exports = {
     insert,
