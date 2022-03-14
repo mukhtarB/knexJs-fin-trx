5bffe1f6319fa0cd8b265832908b7a0a66aa783b
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 06:50:21 +0100

    bug fix on fund wallet

--- api/transactions.js
+++ api/transactions.js
@@ -18,16 +18,18 @@ router.get('/', (req, res) => {
 // user can fund account
 router.post('/deposit', async (req, res) => {
     try {
-        const updated = await update('wallets', 'walletId', req.body.walletId, {amount: req.body.amount});
-        console.log(updated)
+        const wallet = await selectOne('wallets', 'walletId', req.body.walletId);
+        if (!wallet) throw "Can't retrieve wallet details. Check params & retry";
+
+        const updated = await update('wallets', 'walletId', req.body.walletId, {amount: wallet.amount + req.body.amount});
 
         if (!updated || updated == 0) throw "Can't find entry in db. Check params and try again";
 
-        const wallet = await selectOne('wallets', 'walletId', req.body.walletId);
-        console.log(wallet)
+        const updatedWallet = await selectOne('wallets', 'walletId', req.body.walletId);
+
         res.status(200).json({
             msg: 'Wallet funded successfully',
-            currentBalance: wallet.amount
+            currentBalance: updatedWallet.amount
         });
     } catch (error) {
         res.status(500).json(['Internal ServerError', error]);
