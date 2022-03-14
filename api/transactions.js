16688f1dd2a4e3cb884294469ba8c3d4a344eb97
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 08:14:08 +0100

    POST withdrawal endpoint

--- api/transactions.js
+++ api/transactions.js
@@ -82,4 +82,36 @@ router.post('/transfer', async (req, res) => {
 });
 
 
+// user can withdraw from account
+router.post('/withdrawals', async (req, res) => {
+
+    // TO DO:
+    // [x] - retrieve wallet
+    // [x] - update wallet.amount (debit)
+
+    try {
+        const wallet = await selectOne('wallets', 'walletId', req.body.walletId);
+        if (!wallet) throw "Can't retrieve wallet details. Check params & retry";
+
+        if (wallet.amount < req.body.amount) return res.status(200).json({
+            msg: 'Insufficient Balance, Transfer failed',
+            currentBalance: wallet.amount
+        });
+
+        const updated = await update('wallets', 'walletId', req.body.walletId, {amount: wallet.amount - req.body.amount});
+        if (!updated) throw "Can't find entry in db. Check params and try again";
+
+        const updatedWallet = await selectOne('wallets', 'walletId', req.body.walletId);
+
+        res.status(200).json({
+            msg: 'Withdrawals were successfully',
+            currentBalance: updatedWallet.amount
+        });
+    } catch (error) {
+        res.status(500).json(['Internal ServerError', error]);
+    };
+    
+});
+
+
 module.exports = router;
