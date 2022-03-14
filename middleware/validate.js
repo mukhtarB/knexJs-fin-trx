e411ddfd09a22432bc4137b1e61898f55c757e99
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 01:17:36 +0100

    login validation middleware

--- middleware/validate.js
+++ middleware/validate.js
@@ -6,10 +6,19 @@ let validateRegisteration = (req, res, next) => {
         !req.body?.password
     ) return res.status(400).json({
         message: 'All Fields are required'
-    })
+    });
+    next();
+};
+
+let validateLogin = (req, res, next) => {
+    if (!req.body?.email || !req.body?.password)
+    return res.status(400).json({
+        message: 'Email and Password Fields required'
+    });
     next();
 };
 
 module.exports = {
     validateRegisteration,
+    validateLogin
 };
