d80d805fcd34c4f351e5701f5c603f73e345d999
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 02:19:55 +0100

    refactored auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,18 +2,24 @@ const {selectOne} = require('../db/queries');
 
 let isLoggedIn = async (req, res, next) => {
     try {
+        let message = {
+            isLoggedIn: false,
+            errMsg: 'Unauthorized access: Logging and retry'
+        };
+
         let token = req.headers['token'];
+        if (!token) return res.status(401).json(message);
+
         const tokenRes = await selectOne('auth', 'token', token);
 
         if (tokenRes?.code) throw tokenRes;
-        if (!tokenRes) return res.status(401).json({
-            err: 'Unauthorized access',
-            isLoggedIn: false
-        });
+        if (!tokenRes) return res.status(401).json(message);
+        
         next();
+
     } catch (error) {
         res.status(500).json(['Internal ServerError', error]);
     };    
 };
 
-module.exports = {isLoggedIn}
+module.exports = {isLoggedIn};
