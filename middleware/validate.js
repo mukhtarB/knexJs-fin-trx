b786a7b7570f8d2fa7678d9abe09ecafbbca8994
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 01:12:52 +0100

    reg validation middleware

---
+++ middleware/validate.js
@@ -0,0 +1,15 @@
+let validateRegisteration = (req, res, next) => {
+    if (
+        !req.body?.firstName ||
+        !req.body?.lastName ||
+        !req.body?.email ||
+        !req.body?.password
+    ) return res.status(400).json({
+        message: 'All Fields are required'
+    })
+    next();
+};
+
+module.exports = {
+    validateRegisteration,
+};
