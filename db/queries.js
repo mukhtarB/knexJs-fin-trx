93bcd24aaf27ed98d5132b938a5806b8c1b4332d
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 06:37:02 +0100

    update query

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,8 @@ const knex = require('./knex');
  */
 
 const insert = (table, data) => {
-    if (Object.prototype.toString.call(data) !== '[object Object]') throw new Error ('value must be object');
+
+    if (Object.prototype.toString.call(data) !== '[object Object]') throw'data must be of object type';
     
     return knex(`${table}`).insert(data)
     .then( () => knex(`${table}`) )
@@ -23,9 +24,28 @@ const insert = (table, data) => {
  * @returns { Promise<pending> }
  */
 
-const selectOne = (table, col, val) => {
+const selectOne = async (table, col, val) => {
     return knex(table).where(col, val).first()
     .catch(err => err);
+};
+
+
+/**
+ * @param {string} table The Table Name
+ * @param {string} identifierCol The identifier column for the where clause
+ * @param {string} identifierValue The where clause value for specified column
+ * @param {object} identifierValue data
+ * @returns { Promise<pending> }
+ */
+
+ const update = async (table, identifierCol, identifierValue, data) => {
+
+    if (typeof data !== 'object') throw 'data must be an Object';
+    if (!identifierCol) throw "strict mode: where clause needed";
+    if (!identifierValue) throw "strict mode: where clause needs a value";
+
+    return knex(table).where(identifierCol, identifierValue)
+    .update(data)
 };
 
 
@@ -37,13 +57,18 @@ const selectOne = (table, col, val) => {
  */
 
 const del = (table, col, val) => {
+
+    if (!col) throw "strict mode: where clause needed";
+    if (!val) throw "strict mode: where clause needs a value";
+    
     return knex(table)
     .where(col, val).del();
-}
+};
 
 
 module.exports = {
     insert,
     selectOne,
+    update,
     del
 };
