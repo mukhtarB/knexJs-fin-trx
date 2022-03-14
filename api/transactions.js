8ac5d02ac3639f5ff7e28955fdd290a7e088f806
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-14 07:34:18 +0100

    POST Transfer endpoint

--- api/transactions.js
+++ api/transactions.js
@@ -17,12 +17,16 @@ router.get('/', (req, res) => {
 
 // user can fund account
 router.post('/deposit', async (req, res) => {
+
+    // TO DO:
+    // [x] - retrieve wallet
+    // [x] - update wallet.amount (credit)
+
     try {
         const wallet = await selectOne('wallets', 'walletId', req.body.walletId);
         if (!wallet) throw "Can't retrieve wallet details. Check params & retry";
 
         const updated = await update('wallets', 'walletId', req.body.walletId, {amount: wallet.amount + req.body.amount});
-
         if (!updated || updated == 0) throw "Can't find entry in db. Check params and try again";
 
         const updatedWallet = await selectOne('wallets', 'walletId', req.body.walletId);
@@ -36,5 +40,46 @@ router.post('/deposit', async (req, res) => {
     };
     
 });
+
+
+// user can transfer funds to diff account
+router.post('/transfer', async (req, res) => {
+
+    // TO DO:
+    // [x] - retrieve userWallet && receipientWallet
+    // [x] - update userWallet (debit)
+    // [x] - update mreceipentallet (credit)
+
+    try {
+        const userWallet = await selectOne('wallets', 'walletId', req.body.userWalletId);
+        if (!userWallet) throw "Can't retrieve wallets details. Check params & retry";
+        
+        const receipientWallet = await selectOne('wallets', 'walletId', req.body.receipientWalletId);
+        if (!receipientWallet) throw "Can't retrieve wallets details. Check params & retry";
+
+        if (userWallet.amount < req.body.amount) return res.status(200).json({
+            msg: 'Insufficient Balance, Transfer failed',
+            currentBalance: userWallet.amount
+        });
+
+        const updatedWallet = await update('wallets', 'walletId', userWallet.walletId, {amount: userWallet.amount - req.body.amount});
+        if (!updatedWallet) throw "Can't --find entry in db. Check params and try again";
+
+        const updatedWallet_ = await update('wallets', 'walletId', receipientWallet.walletId, {amount: receipientWallet.amount + req.body.amount});
+        if (!updatedWallet_) {
+            // refund any deductions
+            await update('wallets', 'walletId', userWallet.walletId, {amount: userWallet.amount});
+            throw "Server Error: Transfer failed, balance refunded";
+        };
+
+        res.status(200).json({
+            msg: `$${req.body.amount} transferred successfully`,
+            currentBalance: updatedWallet.amount
+        });
+    } catch (error) {
+        res.status(500).json(['Internal ServerError', error]);
+    };
+});
+
 
 module.exports = router;
