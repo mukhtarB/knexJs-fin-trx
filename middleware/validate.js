62b3f0d660ea5fed706cd1ea8fbba78d7a92b2f8
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-16 19:26:20 +0100

    refactored validators

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,39 +1,110 @@
+const express = require('express');
+
+/**
+ * @param {string} field Field containing error
+ * @returns {object} The Error object
+ */
+
+let isRequired = (field) => {return {requiredError: `${field} Field is required`}};
+
+/**
+ * @param {string} field Field containing error
+ * @param {integer} length expected length of field
+ * @returns {object} The Error object
+ */
+
+let hasLength = (field, length) => {return {lenError: `${field} must be an ${length} digits`}};
+
+/**
+ * @param {string} field Field containing error
+ * @param {string} type expected type of field
+ * @returns {object} The Error object
+ */
+
+let isType = (field, type) => {return {typeError: `${field} field must be an ${type}`}};
+
+
+
+/**
+ * Validates the req.body on '/registeration'
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
 const validateRegisteration = (req, res, next) => {
     if (
         !req.body?.firstName ||
         !req.body?.lastName ||
         !req.body?.email ||
         !req.body?.password
-    ) return res.status(400).json({
-        message: 'All Fields are required'
+    ) return res.status(400).json(isRequired('All'));
+
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if (!re.test(req.body.email)) return res.status(400).json({
+        validationError: 'Email must be in email format; xxx@yy.zz'
     });
+
     next();
 };
 
+
+/**
+ * Validates the req.body on "/login" route
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 
 const validateLogin = (req, res, next) => {
+
     if (!req.body?.email || !req.body?.password)
-    return res.status(400).json({
-        message: 'Email and Password Fields required'
-    });
+    return res.status(400).json(isRequired('Email and Password'));
+
     next();
 };
 
+
+/**
+ * Validates the req.body on '/deposit' and '/withdrawals' routes
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 
 const valDepositWithdrawalReqBody = (req, res, next) => {
 
-    if (!req.body?.walletId) return res.status(400).json('WalletId Field is required');
-    if (!req.body?.amount) return res.status(400).json('Amount Field is required');
+    if (!req.body?.amount)
+    return res.status(400).json(isRequired('Amount'));
+
+    if (typeof req.body?.amount !== 'number')
+    return res.status(400).json(isType('amount', 'number'));
 
     next();
 };
 
 
+/**
+ * Validates the req.body on '/transfer'
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
 const valTransferReqBody = (req, res, next) => {
 
-    if (!req.body?.userWalletId) return res.status(400).json('userWalletId Field is required');
-    if (!req.body?.receipientWalletId) return res.status(400).json('receipientWalletId Field is required');
-    if (!req.body?.amount) return res.status(400).json('Amount Field is required');
+    if (!req.body?.receipientWalletId)
+    return res.status(400).json(isRequired('receipientWalletId'));
+
+    if (typeof req.body?.receipientWalletId !== 'number')
+    return res.status(400).json(isType('receipientWalletId', 'integer number'));
+
+    if (req.body?.receipientWalletId.toString().length !== 8)
+    return res.status(400).json(hasLength('receipientWalletId', 8));
+
+    if (!req.body?.amount) return res.status(400).json(isRequired('Amount'));
+    if (typeof req.body?.amount !== 'number') return res.status(400).json(isType('amount', 'number'));
 
     next();
 };
