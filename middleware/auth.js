8e5d5ed6683ecedade70b5b6d03df57fbc3608b7
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-16 19:26:06 +0100

    refactored auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,34 @@
-const {selectOne} = require('../db/queries');
+const express = require('express');
+const { selectOne } = require('../db/queries');
+
+/**
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 
 let isLoggedIn = async (req, res, next) => {
+    let errFn = (errMsg) => {
+        return { isLoggedIn: false, errMsg };
+    };
+    
     try {
-        let message = {
-            isLoggedIn: false,
-            errMsg: 'Unauthorized access: Logging and retry'
-        };
-
         const tokenAuthorization = req.header('Authorization');
-        if (!tokenAuthorization) return res.status(401).json(message);
+        if (!tokenAuthorization) return res.status(401).json(errFn('Unauthorized access: No Authorization'));
 
         const token = tokenAuthorization.slice(7);
 
         const tokenRes = await selectOne('auth', 'token', token);
 
-        if (tokenRes?.code) throw tokenRes;
-        if (!tokenRes) return res.status(401).json(message);
+        if (tokenRes?.errno) throw tokenRes;
+        if (!tokenRes) return res.status(401).json(errFn('Unauthorized access: Authorization failed'));
+
+        res.locals.app_user_id = tokenRes.user_id;
 
         next();
 
     } catch (error) {
-        res.status(500).json(['Internal ServerError', error]);
+        res.status(error).json(['Internal ServerError', error]);
     };
 };
 
