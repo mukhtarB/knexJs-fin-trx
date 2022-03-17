897187a38e968e2fe9207fa3da71ec96e4275c6f
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-17 20:41:29 +0100

    added test suite

---
+++ test/users.test.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test';
+
+
+describe('Users API Route', () => {
+
+    describe('GET Users', () => {
+        it('should return a statement', (done) => {
+
+        });
+    });
+
+});
