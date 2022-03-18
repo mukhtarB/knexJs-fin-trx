d44f76ea5b7b010929dfdf125bc9f9c47606c02a
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-18 16:24:35 +0100

    simple test

--- test/users.test.js
+++ test/users.test.js
@@ -1,11 +1,16 @@
+const app = require('../app');
+const request = require('supertest');
+
 process.env.NODE_ENV = 'test';
 
 
 describe('Users API Route', () => {
 
-    describe('GET Users', () => {
-        it('should return a statement', (done) => {
-
+    describe('GET /Users', () => {
+        it('should return a statement', () => {
+            request(app)
+            .get('/api/v1/users/')
+            .expect(200)
         });
     });
 
