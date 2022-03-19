de5dc5482b8627ede141118612d3ef076d73a847
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-19 07:30:06 +0100

    added test suite

--- test/users.test.js
+++ test/users.test.js
@@ -6,15 +6,4 @@ process.env.NODE_ENV = 'test';
 
 describe('Users API Route', () => {
 
-    describe('GET /Users', () => {
-        it('should return a statement', async () => {
-            const response = await request(app)
-            .get('/api/v1/users/')
-            .expect(200)
-            .expect('Content-Type', 'text/html; charset=utf-8')
-
-            expect(response.text).toBe('users api route: Authentication Logic')
-        });
-    });
-
 });
