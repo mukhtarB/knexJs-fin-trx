77f824d8ce123721f06637c217dc0068e8a9e0a0
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-19 07:55:39 +0100

    refactoring

--- api/__test__/users.test.js
+++ api/__test__/users.test.js
@@ -1,4 +1,4 @@
-const app = require('../app');
+const app = require('../../app');
 const request = require('supertest');
 
 process.env.NODE_ENV = 'test';
@@ -13,6 +13,14 @@ describe('Users API Route', () => {
             .expect(200)
             .expect('Content-Type', 'text/html; charset=utf-8')
             .expect('users api route: Authentication Logic')
+        });
+    });
+
+    describe('POST /register', () => {
+        it('should post data to db', async () => {
+            await request(app)
+            .post('api/v1/users/register')
+            .send
         });
     });
 
