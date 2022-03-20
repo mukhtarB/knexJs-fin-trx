c289b68dbf22feb80358b54fda182830fd6f8872
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-03-20 14:26:17 +0100

    handling setup and clean up with jest hooks

--- api/__test__/users.test.js
+++ api/__test__/users.test.js
@@ -1,5 +1,18 @@
 const app = require('../../app');
 const request = require('supertest');
+const knex = require('../../db/knex');
+
+jest.setTimeout(300000)
+
+beforeAll(async () => {
+    await knex.migrate.rollback();
+    await knex.migrate.latest();
+});
+
+afterAll(async () => {
+    await knex.migrate.rollback();
+    await knex.destroy();
+});
 
 describe('Users API Route', () => {
 
